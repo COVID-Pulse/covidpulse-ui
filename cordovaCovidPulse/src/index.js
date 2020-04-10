969fa614a8e9acd1a4fa4aa214e8c60688eb34b4
Avinash <avinash.umasankaramoorthy@coda.global>
2020-04-10 20:19:35 +0530

    Mobile Setup

--- cordovaCovidPulse/src/index.js
+++ cordovaCovidPulse/src/index.js
@@ -4,13 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const renderReactDom = () => {
+    ReactDOM.render(<App />, document.getElementById('root'));
+};
 
+if (window.cordova) {
+    document.addEventListener('deviceready', () => {
+        renderReactDom();
+    }, false);
+} else {
+    renderReactDom();
+}
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
---
+++ cordovaCovidPulse/www/precache-manifest.a5b6ebdfce3e898e002b712f5a55194f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b062defc252617d24a493a059af909ec",
+    "url": "./index.html"
+  },
+  {
+    "revision": "472d7ec68e136e00daa4",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "43934815287966fec61d",
+    "url": "./static/js/2.fb71790c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.fb71790c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "472d7ec68e136e00daa4",
+    "url": "./static/js/main.7cbdbc77.chunk.js"
+  },
+  {
+    "revision": "fb8d6ceb1e62d6df280f",
+    "url": "./static/js/runtime-main.f1bb74e1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
